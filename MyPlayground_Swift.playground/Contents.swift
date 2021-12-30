8c8e9c9555c0d507ffa082c0d35b77473bdaffa1
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-30 14:03:57 +0900

    Tuples

--- MyPlayground_Swift.playground/Contents.swift
+++ MyPlayground_Swift.playground/Contents.swift
@@ -1,14 +1,19 @@
 import UIKit
 
-// Booleans
+var str = "Hello, playground"
 
-var isOpen = false
+// Tuples (n짝, n쌍)
 
+var topTitle = ("메인화면", "mainIcon.png")
 
+topTitle.0
+topTitle.1
 
-if !isOpen {
-    
-} else {
-    
-}
+// http status code
+var httpError = (statusCode: 404, description: "not found")
 
+//httpError.0
+//httpError.1
+
+httpError.statusCode
+httpError.description
