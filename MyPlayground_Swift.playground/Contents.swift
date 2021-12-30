fffce90ef63817f607508695171ff5040a518d4f
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-30 11:35:28 +0900

    var, let (변수, 상수)

---
+++ MyPlayground_Swift.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// 변수 상수
+
+var greeting = "Hello, playground"
+
+var age = 10
+
+age = 20
+age = 30
+age = 55
+
+greeting = "hello"
+greeting = "hi"
+
+// Swift 언어 특징
+// Safe type 타입 안전한, 강타입, 타입이 엄격하다.
+
+//age = "hi"
+//greeting = 111
+
+// 상수 값이 변하지 않음.
+let guestName = "john"
+
+//guestName = "kim"
+
+let userID = "asdf@gmail.com"
