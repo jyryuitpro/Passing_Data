171e65848970c4c17454605e79550a7e86012f18
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-31 11:11:26 +0900

    Unicode

--- MyPlayground_Swift.playground/Contents.swift
+++ MyPlayground_Swift.playground/Contents.swift
@@ -1,24 +1,21 @@
 import UIKit
 
-//비교 연산자
-//Comparison Operators
+//Unicode 범위관련
 
-let a = "a"
-let b = "b"
+//숫자만 입력받을 수 있어야 한다.
 
-a == b
-a != b
-a > b
-a < b
-a >= b
-a <= b
+let inputValue = "1"
 
-if (a > b) == true {
-    
+"\u{30}"
+"\u{39}"
+
+if inputValue >= "\u{30}" && inputValue <= "\u{39}" {
+    print("숫자 O")
 } else {
-    
+    print("숫자 X")
 }
 
+//영어만 입력해야 한다.
 
-"\u{61}"
-"\u{62}"
+"\u{41}"
+"\u{7a}"
