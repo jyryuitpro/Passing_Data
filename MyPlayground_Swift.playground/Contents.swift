d61d1756d05ec793c9a2fe00be9a3ab19996f56d
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-31 10:38:48 +0900

    Optionals_3

--- MyPlayground_Swift.playground/Contents.swift
+++ MyPlayground_Swift.playground/Contents.swift
@@ -3,24 +3,47 @@ import UIKit
 // Optionals (옵셔널)
 // 값이 있을수도 있고, 없을수도 있다.
 
-// 옵셔널 그냥 사용할 수 없는 경우가 많이 있음.
-
-var a: Int? = 10
-//var b: Int? = 20
+var a: Int? = 20
+//var a: Int?
 var b: Int?
 
-// unwrapped
-// unwrapping
-// unwrap
+//Unwrapping
+//if Statements
 
-// Int? -> Int
-// String? -> String
+//if a != nil {
+//    print(a!)
+//}
 
-// coalesce
-var c = (a ?? 0) + (b ?? 0)
+//if let
+//if var
+//if let hasNumber = a {
+//    //a가 nil이면 실행되지 않는다.
+//    print(hasNumber)
+//}
 
-//var c = (a ?? 0) + b
-// Force-unwrap
-// 주의사항
-var d = a! + b!
+//if var hasNumber = a {
+//    hasNumber = hasNumber * 40
+//    print(hasNumber)
+//}
 
+//guard let
+//guard var
+
+func testFunc1() {
+    guard let hasNumber = a else {
+        return
+    }
+    print(hasNumber)
+    print("end")
+}
+
+func testFunc2() {
+    guard var hasNumber = a else {
+        return
+    }
+    hasNumber = hasNumber * 2
+    print(hasNumber)
+    print("end")
+}
+
+testFunc2()
