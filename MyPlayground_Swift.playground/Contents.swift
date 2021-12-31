92640c0d80637e9213fe4c58343cd93fc7a797ab
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-31 23:05:05 +0900

    Dictionary

--- MyPlayground_Swift.playground/Contents.swift
+++ MyPlayground_Swift.playground/Contents.swift
@@ -3,30 +3,17 @@ import UIKit
 //Collection Types
 //집합 타입
 
-//Set
+//Dictionary
 
-var names = Set<String>()
-names.insert("kim")
-names.insert("min")
-names.insert("lee")
-names.insert("lee")
-names
+var namesOfStreet = [String : Any]()
 
-var sameNames = ["lee", "lee", "kim", "lee"]
-sameNames
-Set(sameNames)
+namesOfStreet["302ro"] = "1st Street"
+namesOfStreet["303ro"] = "2nd Street"
 
-var numbers1: Set = [1, 2, 3, 4, 5]
-var numbers2: Set = [4, 5, 6, 7, 8]
+var nameOfStreet2 = ["a" : 1, "b" : 2, "c": 3]
 
-//교집합
-numbers1.intersection(numbers2)
+namesOfStreet.keys
 
-//합집합
-numbers1.union(numbers2)
-
-//합집합 - 교집합
-numbers1.symmetricDifference(numbers2)
-
-//여집합
-numbers1.subtracting(numbers2)
+for dic in namesOfStreet {
+    print(dic)
+}
