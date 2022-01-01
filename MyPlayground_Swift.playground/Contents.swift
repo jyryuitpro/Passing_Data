18c0b7852868c58fa18d5a33be9893cfc890e3b9
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-01 16:47:18 +0900

    Closure_2

--- MyPlayground_Swift.playground/Contents.swift
+++ MyPlayground_Swift.playground/Contents.swift
@@ -17,4 +17,29 @@ let myScore2 = { (a: Int) -> String in
     return "\(a)점"
 }
 
-myScore2(20)
+//축약 (생략)
+
+//한줄이면...
+let myScore3 = { (a: Int) -> String in
+    "\(a)점"
+}
+
+//return type 추론
+let myScore4 = { (a: Int) in
+    "\(a)점"
+}
+
+
+//type 지정을 한 경우...
+let myScore5: (Int, Int) -> String = { (a, b) in
+    "\(a + b)점"
+}
+
+//in이 없어야 $사용 가능...
+let myScore6: (Int) -> String = {
+    return "\($0)점"
+}
+
+let myScore6F: (Int, Int, Int) -> String = {
+    "\($0 + $1 + $2)점"
+}
