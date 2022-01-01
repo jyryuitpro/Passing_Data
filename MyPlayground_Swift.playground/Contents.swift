d64f1d1340241e2fb90acb90814317df4085f64d
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-01 16:19:56 +0900

    Closure_1

--- MyPlayground_Swift.playground/Contents.swift
+++ MyPlayground_Swift.playground/Contents.swift
@@ -1,45 +1,20 @@
 import UIKit
 
-//Function (함수)
+//Closure (클로저)
+//func - 유사
+//이름이 없음.
 
-//계산기 만들기 -> Class
-//더하기 기능 -> Function
-
-let a = 10
-let b = 20
-
-var c = 0
-
-func plus(_ num1: Int, _ num2: Int) -> Int {
-    return num1 + num2
+//function
+func myScore(a: Int) -> String {
+    return "\(a)점"
 }
 
-func minus(_ num1: Int, _ num2: Int) -> Int {
-    return num1 - num2
+let score = myScore
+score(50)
+
+//Closure
+let myScore2 = { (a: Int) -> String in
+    return "\(a)점"
 }
 
-func miltiply(_ num1: Int, _ num2: Int) -> Int {
-    return num1 * num2
-}
-
-var inputButtonType = "+"
-
-if inputButtonType == "+" {
-    print("연산 결과", plus(a, b))
-} else if inputButtonType == "-" {
-    print("연산 결과", minus(a, b))
-} else if inputButtonType == "*" {
-    print("연산 결과", miltiply(a, b))
-}
-
-func displayCalc(result: (Int, Int) -> Int) {
-    print("연산 결과", result(a, b))
-}
-
-if inputButtonType == "+" {
-    displayCalc(result: plus)
-} else if inputButtonType == "-" {
-    displayCalc(result: minus)
-} else if inputButtonType == "*" {
-    displayCalc(result: miltiply)
-}
+myScore2(20)
