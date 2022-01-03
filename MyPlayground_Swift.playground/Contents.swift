a0e1e771f10378326c168dd203627c89c86bbde9
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-03 22:17:06 +0900

    Class_1

--- MyPlayground_Swift.playground/Contents.swift
+++ MyPlayground_Swift.playground/Contents.swift
@@ -1,48 +1,31 @@
 import UIKit
 
-//Enumerations
+//Class
 
-//enum
-//타입분류
-
-//도서관 -> 항목 -> 소설, 문제집, 패션, 만화책
-
-//분류만 하고 싶다.
-enum BookType {
-    case fiction(title: String, price: Int, year: Int)
-    case comics(title: String, price: Int, year: Int)
-    case workbook(title: String, price: Int, year: Int)
-}
-
-extension BookType {
-    var typeName: String {
-        switch self {
-        case .comics:
-            return "comics"
-        case .fiction:
-            return "fiction"
-        case .workbook:
-            return "workbook"
+class MyInfo {
+    init(gender: GenderType) {
+        self.genderType = gender
+    }
+    enum GenderType {
+        case male
+        case female
+    }
+    
+    private var genderType: GenderType
+    
+    var name = ""
+    var age = 0
+    
+    func isAdult() -> Bool {
+        if age > 19 {
+            return true
         }
+        return false
     }
 }
 
-//var bookStyle = BookType.comics
-
-var bookStyle: BookType?
-
-var books = [BookType]()
-
-func saveBook(book: BookType) {
-    books.append(book)
-}
-
-saveBook(book: .comics(title: "aaa", price: 5000, year: 2020))
-saveBook(book: .comics(title: "aaa", price: 5000, year: 2020))
-saveBook(book: .comics(title: "aaa", price: 5000, year: 2020))
-
-for book in books {
-    if case let BookType.comics(title, _, _) = book {
-        print(title, book.typeName)
-    }
-}
+//참조 (class are reference type)
+var myInfo = MyInfo(gender: .female)
+myInfo.age = 20
+var myInfo2 = myInfo
+myInfo2.age
