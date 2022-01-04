a7bf086c9dcdcfc031e75e2dd04ed513bd111a02
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-05 08:34:40 +0900

    App Build Intro 2

---
+++ App_Start/App_Start/DetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailVC.swift
+//  App_Start
+//
+//  Created by 류지영 on 2022/01/05.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
--- App_Start/App_Start/ViewController.swift
+++ App_Start/App_Start/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var testButton: UIButton!
+    
+    @IBAction func doSomething(_ sender: Any) {
+        testButton.backgroundColor = .orange
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let detailVC = storyboard.instantiateViewController(identifier: "DetailVC") as DetailVC
+        
+        self.present(detailVC, animated: true, completion: nil)
+    }
+    
+    //View Controller life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        testButton.backgroundColor = UIColor.red
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
 }
 
