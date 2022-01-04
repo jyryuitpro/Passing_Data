4640fac235b7df0a9f575d0812ef6e3019bd94e7
jyryuitpro <jy.ryu.itpro@gmail.com>
2022-01-05 00:54:38 +0900

    App Build Intro 1

---
+++ App_Start/App_Start/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_Start
+//
+//  Created by 류지영 on 2022/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
